b2374e387166d9f815def7be7d82c4e236ea0a19
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-04-14 13:38:38 -0700

    Updated components and added a new jsx file

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,12 @@
 import { Box } from "@mui/material";
 import Navbar from "scenes/navbar";
 
+
+
 const HomePage = () => {
-    return(
-        <Box>
-            <Navbar />
-        </Box>
-    );
+    return <Box>
+        <Navbar />
+    </Box>;
 };
+
 export default HomePage;
--- server/index.js
+++ server/index.js
@@ -34,15 +34,14 @@ app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, file.originalname);
+    destination: function (req, file, cb) {
+      cb(null, "public/assets");
     },
-    filename: function (req, file, cb){
-        cancelAnimationFrame(null, file.originalname);
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
     },
-});
-const upload = multer ({ storage });
-
+  });
+  const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
