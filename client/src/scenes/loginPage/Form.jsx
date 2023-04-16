3a3f872ee11f5f8ab319d055b6f0fb33fbf1921a
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-04-16 11:04:59 -0700

    One more final change

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -266,6 +266,7 @@ const Form = () => {
               {isLogin
                 ? "Don't have an account? Sign Up here."
                 : "Already have an account? Login here."}
+                
             </Typography>
           </Box>
         </form>
